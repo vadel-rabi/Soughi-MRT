cfbc12f399dafd1def009db43fcce36fe1e69694
vadel-rabi <vadelrb650@gmail.com>
2025-06-29 20:54:46 +0000

    PostAd.js

---
+++ Public/src/Pages/PostAd.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PostAd() {
+  return (
+    <div>
+      <h2>Post a New Ad</h2>
+      <input placeholder="Title" /><br />
+      <textarea placeholder="Description" /><br />
+      <button>Submit Ad</button>
+    </div>
+  );
+}
+
+export default PostAd;
