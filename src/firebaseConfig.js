1958a31f263cfe4ed104f5da1ec18d9d4b89b793
vadel-rabi <vadelrb650@gmail.com>
2025-06-29 21:21:38 +0000

    firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// You can add your Firebase credentials here later
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+export default firebaseConfig;
