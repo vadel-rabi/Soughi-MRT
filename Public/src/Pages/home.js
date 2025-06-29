12aa80546f91b76629e7069fd21e0460980a56e1
vadel-rabi <vadelrb650@gmail.com>
2025-06-29 20:52:26 +0000

    home.js

---
+++ Public/src/Pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h2>Welcome to Soughi MRT</h2>
+      <p>Buy and sell anything across Mauritania 🇲🇷</p>
+    </main>
+  );
+}
+
+export default Home;
