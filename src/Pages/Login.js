115bc813756a25c6676324061294e25220793c6d
vadel-rabi <vadelrb650@gmail.com>
2025-06-29 21:23:14 +0000

    Login.js

---
+++ src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" /><br />
+      <input placeholder="Password" type="password" /><br />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
